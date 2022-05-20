680ede664d5f1769a7b32705273ee4dbe1b434b8
archristian18 <105032392+archristian18@users.noreply.github.com>
2022-05-20 15:29:42 +0800

    change

---
+++ app/Http/Controllers/sample.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class sample extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return view('index');
+    }
+
+    public function game()
+    {
+       return view('newcontent');
+    }
+
+
+    public function login()
+    {
+       return view('login');
+    }
+
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $x = sample::find($id);
+        return view('index')->with('variable', $x);
+    }
+
+    public function check(Request $request)
+    {
+        $input = $request->all();
+        Student::create($input);
+        return redirect('game')->with('flash_message', 'Student Addedd!');  
+        // 'student is the directory in folder /student in ROUTE:'
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/newcontent.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+    
+@section ('content')
+
+
+<p>yehoy</p>
+
+
+@endsection ('content')
